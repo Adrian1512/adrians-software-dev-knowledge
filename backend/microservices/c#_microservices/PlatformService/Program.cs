9ee6a0f784eb05373ae9b7c7484b0e26684555a0
aobando123 <cobandol@ucenfotec.ac.cr>
2023-03-19 14:22:53 -0600

    platform service startup

--- backend/microservices/c#_microservices/PlatformService/Program.cs
+++ backend/microservices/c#_microservices/PlatformService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +25,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+PrepDb.PrepPopulation(app);
 
 app.Run();
